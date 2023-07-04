71b70dfedcdc031079527b8fa3de916ed460be4e
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2023-07-04 20:13:54 +0900

    📚 Lanjut Belajar PHP

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="border border-gray-800 container mx-auto p-6 text-center">
+    <p>
+        <span>Copyright &copy; 2023. SaCode Developed by <a href="#" class="font-semibold">Billy Rumbiak</a></span>
+    </p>
+</footer>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SaCode New's</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+        <!-- HEADER START -->
+
+        <?php include('includes/header.php') ?>
+
+        <!-- HEADER END -->
+
+        <?php $kategori = $_GET['page'] ?>
+        <!-- NAV START -->
+
+        <?php include('includes/nav.php') ?>
+
+        <!-- NAV END -->
+
+        <!-- MAIN CONTENT START -->
+
+        <section class="border border-gray-800 container mx-auto p-6 text-center">
+            <h1 class="font-mono text-4xl">Katergori <?= $kategori ?></h1>
+        </section>
+
+        <!-- MAIN CONTENT END -->
+
+        <!-- FOOTER START -->
+
+        <?php include('includes/footer.php') ?>
+
+        <!-- FOOTER END -->
+
+</body>
+</html>
